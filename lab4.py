f008ad8a60e97c6754081cec98a0a0aa54201457
Roman Gultaichuk <roman_teron@mail.ru>
2018-04-01 16:57:56 +0300

    Questions for 4th lab prepared.

---
+++ lab4.py
@@ -0,0 +1,5 @@
+# Ask:
+# 1. What is meant by dt (temperature change) and t (temperature)? Comfort temperature
+# median and range in which it can be still comfortable?
+# 2. What const temperature gives oven exactly, when working on half of its power? Given
+# temperature t or it just saves previously set value?
